d2a1253031856ba1342fefa39cf86ca221f1c973
Christopher Nethercott <ccnethercott@gmail.com>
2022-03-08 14:13:52 +0000

    feat: add command framework

--- src/main/java/net/imperialvalor/titanapi/TitanAPI.java
+++ src/main/java/net/imperialvalor/titanapi/TitanAPI.java
@@ -1,5 +1,7 @@
 package net.imperialvalor.titanapi;
 
+import co.aikar.commands.BungeeCommandManager;
+import net.imperialvalor.titanapi.command.TitanCommand;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public final class TitanAPI extends Plugin {
@@ -7,6 +9,9 @@ public final class TitanAPI extends Plugin {
 	@Override
 	public void onEnable() {
 		// Plugin startup logic
+		BungeeCommandManager manager = new BungeeCommandManager(this);
+
+		manager.registerCommand(new TitanCommand());
 	}
 
 	@Override
