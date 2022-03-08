7bc73e7a166daf712f555a463917c668645261fb
Christopher Nethercott <ccnethercott@gmail.com>
2022-03-08 15:51:50 +0000

    feat: add login event

--- src/main/java/net/imperialvalor/titanapi/TitanAPI.java
+++ src/main/java/net/imperialvalor/titanapi/TitanAPI.java
@@ -2,6 +2,7 @@ package net.imperialvalor.titanapi;
 
 import co.aikar.commands.BungeeCommandManager;
 import net.imperialvalor.titanapi.command.TitanCommand;
+import net.imperialvalor.titanapi.event.PlayerJoinEvent;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public final class TitanAPI extends Plugin {
@@ -9,10 +10,15 @@ public final class TitanAPI extends Plugin {
 	@Override
 	public void onEnable() {
 		// Plugin startup logic
+
+		// Register Commands
 		BungeeCommandManager manager = new BungeeCommandManager(this);
-		
+
 		manager.registerCommand(new TitanCommand());
 		getLogger().info("Successfully registered TitanAPI administration commands.");
+
+		// Register Events
+		getProxy().getPluginManager().registerListener(this, new PlayerJoinEvent(this));
 	}
 
 	@Override
