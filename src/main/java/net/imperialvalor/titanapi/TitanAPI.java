412a8cf616839a439bda96891d67a5f1bf9231ad
Christopher Nethercott <ccnethercott@gmail.com>
2022-03-08 13:37:10 +0000

    feat: create initial plugin

---
+++ src/main/java/net/imperialvalor/titanapi/TitanAPI.java
@@ -0,0 +1,16 @@
+package net.imperialvalor.titanapi;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class TitanAPI extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
